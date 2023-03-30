d5e876b0da2d0571caef6c162b30ad8f3adf803a
Owen Lynch <root@owenlynch.org>
2023-03-30 15:53:00 -0700

    PERF: names can use symbols instead of strings

--- src/util/Names.jl
+++ src/util/Names.jl
@@ -1,20 +1,36 @@
 module Names
 export Name, Literal, Anon
 
+"""
+Names are used to label parts of a GAT.
+
+They are used for both human input and output of a GAT, but are not used
+internally.
+"""
 abstract type Name end
-struct Literal <: Name
+
+struct StrLit <: Name
   name::String
 end
 
-Name(n::String) = Literal(n)
-Name(n::Char) = Literal(string(n))
-Name(n::Symbol) = Literal(string(n))
+"""
+We have a symbol wrapper because we get symbols from parsing, and it
+is faster to compare symbols than it is to compare strings.
+"""
+struct SymLit <: Name
+  name::Symbol
+end
+
+Name(n::String) = StrLit(n)
+Name(n::Char) = StrLit(string(n))
+Name(n::Symbol) = SymLit(n)
 Name(n::Name) = n
 
 struct Anon <: Name
 end
 
-Base.string(n::Literal) = n.name
-Base.string(::Anon) = " "
+Base.string(n::StrLit) = n.name
+Base.string(n::SymLit) = string(n.name)
+Base.string(::Anon) = "_"
 
 end
