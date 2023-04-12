8ee75227250b278b882cbf36d2fc6f270ba864c1
Owen Lynch <root@owenlynch.org>
2023-04-12 13:00:35 -0700

    the ideal syntax for algebra

---
+++ src/stdlib/Algebra.jl
@@ -0,0 +1,42 @@
+module Algebra
+
+@theory ThSet <: ThEmpty begin
+  @default::TYPE
+end
+
+@theory ThMagma <: ThClass begin
+  (x ⋅ y) ⊣ [x,y]
+end
+
+@theory ThSemiGroup <: ThMagma begin
+  (x ⋅ y) ⋅ z == (x ⋅ (y ⋅ z)) ⊣ [x,y,z]
+end
+
+@theory ThMonoid <: ThSemiGroup begin
+  e() ⊣ []
+  e() ⋅ x == x ⊣ [x]
+  x ⋅ e() == x ⊣ [x]
+end
+
+@theory ThGroup <: ThMonoid begin
+  i(x) ⊣ [x]
+  i(x) ⋅ x == e ⊣ [x]
+  x ⋅ i(x) == e ⊣ [x]
+end
+
+@theory ThCMonoid <: ThMonoid begin
+  a ⋅ b == b ⋅ a ⊣ [a,b]
+end
+
+@theory ThAb <: ThMonoid begin
+  using ThGroup
+  using ThCMonoid
+end
+
+@theory ThRing <: ThSet begin
+  using ThAb: ⋅ as +, e as 0
+  using ThMonoid
+  a ⋅ (b + c) == (a ⋅ b) + (a ⋅ c) ⊣ [a,b,c]
+end
+
+end
