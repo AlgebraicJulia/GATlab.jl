ca540554e23ddcba7f428eaa67c1c0fa817170dc
Owen Lynch <root@owenlynch.org>
2023-04-13 09:58:44 -0700

    started lenses

---
+++ src/stdlib/Lenses.jl
@@ -0,0 +1,22 @@
+module Lenses
+
+struct SimpleArena
+  pos::Context
+  dir::Context
+end
+
+"""
+expose is a Kleisli map from codom.pos to dom.pos
+update is a Kleisli map from dom.dir to codom.dir + dom.pos
+"""
+struct SimpleKleisliLens
+  dom::SimpleArena
+  codom::SimpleArena
+  expose::Vector{Trm}
+  update::Vector{Trm}
+end
+
+struct SimpleLensC{T<:AbstractTheory} <: Model{ThStrictMonCat}
+end
+
+end
