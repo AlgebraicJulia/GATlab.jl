e5ffde2e66e41cc6e7c9cb747e8edc704cebc519
Owen Lynch <root@owenlynch.org>
2023-04-20 14:19:28 -0700

    allow parameters for conversion to ODESystem

--- src/stdlib/models/MTKInterop.jl
+++ src/stdlib/models/MTKInterop.jl
@@ -25,15 +25,15 @@ end
 
 @simple_model ThRing NumR Impl
 
-function ModelingToolkit.ODESystem(v::SimpleKleisliLens; name)
-  length(v.codom.pos) == length(v.codom.dir) == 0 || error("Cannot integrate an open systems")
+function ModelingToolkit.ODESystem(v::SimpleKleisliLens; tspan, name)
   length(v.dom.dir) == length(v.dom.pos) || error("Expected domain to be a tangent bundle")
   @variables t
   D = Differential(t)
   state_vars = [(@variables $x(t))[1] for x in map(nt -> Symbol(nt[1]), v.dom.pos.ctx)]
-  derivatives = interpret(NumR(), v.morphism.update, state_vars)
+  param_vars = [(@variables $p)[1] for p in map(nt -> Symbol(nt[1]), v.codom.dir.ctx)]
+  derivatives = interpret(NumR(), v.morphism.update, vcat(state_vars, param_vars))
   eqs = [D(x) ~ dx for (x,dx) in zip(state_vars, derivatives)]
-  ODESystem(eqs; name)
+  ODESystem(eqs, t, state_vars, param_vars; tspan, name)
 end
 
 end
