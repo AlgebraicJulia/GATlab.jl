48c87ab5555975a12de50838ae7e5637bbcdb672
Owen Lynch <root@owenlynch.org>
2023-04-21 10:53:40 -0700

    added test to test file

--- test/stdlib/Serialization.jl
+++ test/stdlib/Serialization.jl
@@ -4,6 +4,19 @@ using Test
 
 using Gatlab
 
+periodic_params = @lens ThRing begin
+  dom = [β, γ] | [dβ, dγ]
+  codom = [β, γ] | [β₀, kᵦ, γ₀, kᵧ]
+  expose = begin
+    β = β
+    γ = γ
+  end
+  update = begin
+    dβ = -kᵦ*(β + (-β₀))
+    dγ = -kᵧ*(γ + (-γ₀))
+  end
+end
 
+@test parse_json(JSON.parse(JSON.json(periodic_params)), SimpleKleisliLens{ThRing}) == periodic_params
 
 end
