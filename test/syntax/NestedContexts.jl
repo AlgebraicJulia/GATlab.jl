d63f22d629e5250427768671718d571a8b25f7f0
Owen Lynch <root@owenlynch.org>
2023-05-23 18:44:46 -0700

    started work on nested contexts and new models

---
+++ test/syntax/NestedContexts.jl
@@ -0,0 +1,26 @@
+module TestNestedContexts
+
+using Test, Gatlab
+
+nctx = NestedContext()
+
+dctx = DependentContext(
+  NestedContext(),
+  NestedContext()
+)
+
+push!(dctx, SymLit(:dom), Typ(Lvl(1)))
+push!(dctx, SymLit(:codom), Typ(Lvl(1)))
+
+bare_hom = DependentContext(
+  copy(dctx.context),
+  NestedContext()
+)
+
+push!(bare_hom, SymLit(:it), Typ(Lvl(2), [Trm(Lvl(1; argument=true)), Trm(Lvl(2; argument=true))]))
+
+push!(dctx, SymLit(:f), bare_hom, [Lvl(1; context=true), Lvl(2; context=true)])
+
+println(dctx)
+
+end
