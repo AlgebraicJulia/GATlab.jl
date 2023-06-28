c9d22eed97ab21e520e5c6afe4eae86268645ceb
Owen Lynch <root@owenlynch.org>
2023-06-28 16:56:57 -0700

    fixed renaming of NumR

--- ext/GatlabSciMLExt/GatlabSciMLExt.jl
+++ ext/GatlabSciMLExt/GatlabSciMLExt.jl
@@ -29,7 +29,7 @@ function ModelingToolkit.ODESystem(v::SimpleKleisliLens; name)
   D = Differential(t)
   state_vars = [(@variables $x(t))[1] for x in map(nt -> Symbol(nt[1]), v.dom.pos.ctx)]
   param_vars = [(@parameters $p)[1] for p in map(nt -> Symbol(nt[1]), v.codom.dir.ctx)]
-  derivatives = interpret.(Ref(NumR()), v.morphism.update, Ref(vcat(state_vars, param_vars)))
+  derivatives = interpret.(Ref(RealModel()), v.morphism.update, Ref(vcat(state_vars, param_vars)))
   eqs = [D(x) ~ dx for (x,dx) in zip(state_vars, derivatives)]
   ODESystem(eqs, t, state_vars, param_vars; name)
 end
