9ec4bd6b85cf7fc229d0a599b8f6c2769ef04ddb
Owen Lynch <root@owenlynch.org>
2023-08-30 15:30:04 -0700

    @withmodel, more tests
    Co-authored-by: Kris Brown <kris-brown@users.noreply.github.com>

--- test/models/MethodInstance.jl
+++ test/models/MethodInstance.jl
@@ -1,28 +1,6 @@
 module TestMethodInstance 
 using Gatlab
 
-struct FinSet 
-  n::Int 
-end
 
-struct FinFunction 
-  values::Vector{Int}
-  dom::FinSet 
-  codom::FinSet 
-end 
-
-@instance ThCategory{FinSet,FinFunction} begin 
-  dom(f::FinFunction) = f.dom 
-  codom(f::FinFunction) = f.codom 
-  id(A::FinSet) = FinFunction(1:A.n, A, A)
-  compose(f::FinFunction, g::FinFunction) = FinFunction(g.values[f.values], dom(f),codom(g))
-end
-
-A = FinSet(2); 
-B=FinSet(3)
-f = FinFunction([2,3],A,B)
-g = FinFunction([1,1,1],B,A)
-id(A)
-⋅(f,g)
 
 end # module
--- test/models/ModelInterface.jl
+++ test/models/ModelInterface.jl
@@ -2,6 +2,7 @@ module TestModelInterface
 
 using Gatlab
 using Test
+using StructEquality
 
 struct FinSetC <: Model{Tuple{Int, Vector{Int}}}
 end
@@ -49,5 +50,68 @@ using .ThStrictMonCat
 
 @test mcompose(id(2; model=FinSetC()), id(2; model=FinSetC()); context=(;B₁=2), model=FinSetC()) ==
   id(4; model=FinSetC())
+
+@struct_hash_equal struct FinSet 
+  n::Int 
+end
+
+@struct_hash_equal struct FinFunction 
+  values::Vector{Int}
+  dom::FinSet 
+  codom::FinSet 
+end 
+
+@instance ThCategory{FinSet,FinFunction} begin 
+  dom(f::FinFunction) = f.dom 
+  codom(f::FinFunction) = f.codom
+  
+  id(A::FinSet) = FinFunction(1:A.n, A, A)
+  compose(f::FinFunction, g::FinFunction) = FinFunction(g.values[f.values], dom(f),codom(g))
+end
+
+A = FinSet(2)
+B = FinSet(3)
+f = FinFunction([2,3],A,B)
+g = FinFunction([1,1,1],B,A)
+@test id(A) == FinFunction([1,2], A, A)
+@test compose(f,g) == FinFunction([1,1], A, A)
+
+@withmodel FinSetC() (mcompose, id) begin
+  @test mcompose(id(2), id(2); context=(;B₁=2)) == id(4)
+end
+
+@test_throws UndefKeywordError id(2) # the macro does not globally overload methods
+
+@test_throws LoadError @eval @instance ThCategory{Int, Vector{Int}} (;model::FinSetC) begin
+end
+
+@test_throws LoadError @eval @instance ThCategory{Int, Vector{Int}} (;model::FinSetC) begin
+  compose(f::Vector{Int}, g::Vector{Int}) = g[f]
+  dom(f::Vector{Int}) = length(f)
+end
+
+@test_throws LoadError @eval @instance ThCategory{Int, Vector{Int}} (;model::FinSetC) begin
+  id(f::Vector{Int}) = f
+  compose(f::Vector{Int}, g::Vector{Int}) = g[f]
+  dom(f::Vector{Int}) = length(f)
+end
+
+
+@test_throws LoadError @eval @instance ThStrictMonCat{Int, Vector{Int}} (;model::FinSetC) begin
+  @import Ob, Hom, id, compose, dom, codom
+
+  mcompose(f::Vector{Int}, g::Vector{Int}; context) = [f; g .+ context.B₁]
+
+  munit() = 0
+end
+
+@test_throws LoadError @eval @instance ThStrictMonCat{Int, Int} (;model::FinSetC) begin
+  @import Ob, Hom, id, compose, dom, codom
+
+  mcompose(a::Int, b::Int) = a + b
+  mcompose(f::Int, g::Int; context) = f * g
+
+  munit() = 0
+end
 
 end # module
