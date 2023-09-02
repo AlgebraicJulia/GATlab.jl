a1d3c7182fbe76f558ca2b69fd9f2421ca1f0bbd
Owen Lynch <root@owenlynch.org>
2023-09-01 18:55:18 -0700

    working towards codecov target, rationalized Scopes API
    colored identifiers
    
    started reworking ExprInterop
    
    finished cleanup

--- src/syntax/Presentations.jl
+++ src/syntax/Presentations.jl
@@ -1,6 +1,8 @@
-module Presentations 
-export Presentation, getscope
-using ..Scopes, ..GATs
+module Presentations
+export Presentation
+
+using ...Util
+using ..Scopes, ..GATs, ..ExprInterop
 using StructEquality 
 
 """
@@ -8,7 +10,7 @@ A presentation has a set of generators, given by a `TypeScope`, and a set of
 equations among terms which can refer to those generators. Each element of 
 `eqs` is a list of terms which are asserted to be equal.
 """
-@struct_hash_equal struct Presentation
+@struct_hash_equal struct Presentation <: HasContext
   theory::GAT
   scope::TypeScope
   eqs::Vector{Vector{AlgTerm}}
@@ -25,6 +27,46 @@ equations among terms which can refer to those generators. Each element of
   end
 end
 
-getscope(p::Presentation) = AppendScope(p.theory, p.scope)
+getcontext(p::Presentation) = AppendScope(p.theory, p.scope)
+
+"""Context of presentation is the underlying GAT"""
+ExprInterop.toexpr(p::Presentation) = toexpr(p.theory, p)
+
+function ExprInterop.toexpr(c::Context, p::Presentation)
+  c == p.theory || error("Invalid context for presentation")
+  decs = GATs.bindingexprs(c, p.scope)
+  eqs = map(p.eqs) do ts
+    exprs = zip(toexpr.(Ref(p), ts),Iterators.repeated(:(==)))
+    Expr(:comparison, collect(Iterators.flatten(exprs))[1:(end-1)]...)
+  end
+  Expr(:block, [decs..., eqs...]...)
+end
+
+"""
+Parse, e.g.,  (a,b,c)::Ob
+              f::Hom(a, b)
+              g::Hom(b, c)
+              h::Hom(a, c)
+              h′::Hom(a, c)
+              compose(f, g) == h == h′
+"""
+function ExprInterop.fromexpr(c::Context, e, ::Type{Presentation})
+  e.head == :block || error("expected a block to parse into a GATSegment, got: $e")
+  scopelines, eqlines = Expr0[], Vector{Expr0}[]
+  for line in e.args
+    if line.head == :(==)
+      push!(eqlines, line.args)
+    elseif line.head == :comparison
+      er = "Bad comparison: $line"
+      all(((i,v),)-> iseven(i) == (v == :(==)), enumerate(line.args)) || error(er)
+      push!(eqlines, line.args[1:2:end])
+    else
+      push!(scopelines, line)
+    end
+  end
+  scope = GATs.parsetypescope(c, scopelines)
+  apscope = AppendScope(c, scope)
+  Presentation(c, scope, [fromexpr.(Ref(apscope), ts, AlgTerm) for ts in eqlines])
+end
 
 end # module 
