7f7facb44d7b8c16578f02c53efb6c58eb39f433
Owen Lynch <root@owenlynch.org>
2023-09-05 17:11:33 -0700

    throw error on domain/codomain clash for compose

--- test/models/SymbolicModels.jl
+++ test/models/SymbolicModels.jl
@@ -8,7 +8,7 @@ abstract type ObExpr{T} <: CategoryExpr{T} end
 
 abstract type HomExpr{T} <: CategoryExpr{T} end
 
-@symbolic_model FreeCategory{ObExpr, HomExpr} ThCategory begin
+@macroexpand @symbolic_model FreeCategory{ObExpr, HomExpr} ThCategory begin
 end
 
 x, y = FreeCategory.Ob{:generator}([:x], []), FreeCategory.Ob{:generator}([:y], [])
@@ -17,5 +17,6 @@ f = FreeCategory.Hom{:generator}([:f], [x, y])
 @test x isa ObExpr{:generator}
 @test ThCategory.id(x) isa HomExpr{:id}
 @test ThCategory.compose(ThCategory.id(x), f) isa HomExpr{:compose}
+@test_throws SyntaxDomainError ThCategory.compose(f, f)
 
 end
