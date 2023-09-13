ddc2ec56f53902564ae138bcdb716394fa84214b
Kris Brown <kris@topos.institute>
2023-09-13 13:51:16 -0700

    op

--- test/stdlib/models/FinSets.jl
+++ test/stdlib/models/FinSets.jl
@@ -4,7 +4,7 @@ using GATlab, Test
 
 using .ThCategory
 
-@withmodel FinSetC() (Ob, Hom, id, compose, dom) begin
+@withmodel FinSetC() (Ob, Hom, id, compose, dom, codom) begin
   @test Ob(0) == 0
   @test_throws TypeCheckFail Ob(-1)
   @test_throws TypeCheckFail Hom([1,5,2], 3, 4)
@@ -13,6 +13,7 @@ using .ThCategory
   @test id(2) == [1,2]
   @test compose([5], [1,1,1,3,2]) == [2]
   @test dom([5]) == 1
+  @test codom([5]; context=(c=10,)) == 10
 end
 
 end
---
+++ test/stdlib/models/Op.jl
@@ -0,0 +1,20 @@
+"""Same as FinSetC tests but with all doms/codoms reversed"""
+module TestOp
+
+using GATlab, Test
+
+using .ThCategory
+
+@withmodel op(FinSetC()) (Ob, Hom, id, compose, dom, codom) begin
+  @test Ob(0) == 0
+  @test_throws TypeCheckFail Ob(-1)
+  @test_throws TypeCheckFail Hom([1,5,2], 4, 3)
+  @test Hom(Int[], 4, 0) == Int[]
+
+  @test id(2) == [1,2]
+  @test compose([1,1,1,3,2], [5]) == [2]
+  @test codom([5]) == 1
+  @test dom([5]; context=(c=10,)) == 10
+end
+
+end # module
