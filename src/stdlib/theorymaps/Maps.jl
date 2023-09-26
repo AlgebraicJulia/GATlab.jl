785ae67f2820be773cfaea388850490962c3cf10
Kris Brown <kris@topos.institute>
2023-09-26 16:17:06 -0700

    refactor gatmorphisms

--- src/stdlib/theorymaps/Maps.jl
+++ src/stdlib/theorymaps/Maps.jl
@@ -5,7 +5,7 @@ export SwapMonoid, NatPlusMonoid, PreorderCat, OpCat
 using ...StdTheories
 using ....Syntax
 
-
+using GATlab
 SwapMonoid = @theorymap ThMonoid => ThMonoid begin
   default => default
   x⋅y ⊣ [x, y] => y⋅x
@@ -22,16 +22,16 @@ end
 
 OpCat = @theorymap ThCategory => ThCategory begin
   Ob => Ob
-  Hom(dom::Ob, codom::Ob) => Hom(codom, dom)
-  id(a::Ob) => id(a)
-  compose(f::Hom(a,b), g::Hom(b,c)) ⊣ [(a,b,c)::Ob] => compose(g, f)
+  Hom(dom, codom) ⊣ [dom::Ob, codom::Ob] => Hom(codom, dom)
+  id(a) ⊣ [a::Ob] => id(a)
+  compose(f,g) ⊣ [(a,b,c)::Ob, f::Hom(a,b), g::Hom(b,c)] => compose(g, f)
 end
 
 """Preorders are categories"""
 PreorderCat = @theorymap ThCategory => ThPreorder begin
   Ob => default
-  Hom(dom::Ob, codom::Ob) => Leq(dom, codom)
-  compose(f::(a → b), g::(b → c)) ⊣ [a::Ob, b::Ob, c::Ob] => trans(f, g)
+  Hom(dom, codom) ⊣ [dom::Ob, codom::Ob] => Leq(dom, codom)
+  compose(f, g) ⊣ [a::Ob, b::Ob, c::Ob, f::(a → b), g::(b → c)] => trans(f, g)
   id(a) ⊣ [a::Ob] => refl(a)
 end
 
