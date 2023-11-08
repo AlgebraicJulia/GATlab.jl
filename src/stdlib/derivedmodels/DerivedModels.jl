6e4972c0826dddaa4f71fbb5f85c3abc64338406
Kevin Arlin <arlinkevind@gmail.com>
2023-11-08 15:31:16 -0800

    migrate_theory to migrate_model

--- src/stdlib/derivedmodels/DerivedModels.jl
+++ src/stdlib/derivedmodels/DerivedModels.jl
@@ -6,12 +6,12 @@ using ...StdTheoryMaps
 using ...StdModels
 
 # Given a model of a category C, we can derive a model of Cᵒᵖ.
-@migrate_theory OpFinSetC = OpCat(FinSetC)
+@migrate_model OpFinSetC = OpCat(FinSetC)
 
 # Interpret `e` as `0` and `⋅` as `+`.
-@migrate_theory IntMonoid = NatPlusMonoid(IntNatPlus)
+@migrate_model IntMonoid = NatPlusMonoid(IntNatPlus)
 
 # Interpret `id` as reflexivity and `compose` as transitivity.
-@migrate_theory IntPreorderCat = PreorderCat(IntPreorder)
+@migrate_model IntPreorderCat = PreorderCat(IntPreorder)
 
 end
