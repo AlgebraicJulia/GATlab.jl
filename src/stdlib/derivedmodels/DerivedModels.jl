345cc443a4dd4d62286052d415aba82f324d2ae3
Kris Brown <kris@topos.institute>
2023-11-11 07:29:25 +0530

    migrate -> migrate_theory

--- src/stdlib/derivedmodels/DerivedModels.jl
+++ src/stdlib/derivedmodels/DerivedModels.jl
@@ -6,12 +6,12 @@ using ...StdTheoryMaps
 using ...StdModels
 
 # Given a model of a category C, we can derive a model of Cᵒᵖ.
-OpFinSetC = migrate(OpCat, FinSetC())
+OpFinSetC = migrate_theory(OpCat, FinSetC())
 
 # Interpret `e` as `0` and `⋅` as `+`.
-IntMonoid = migrate(NatPlusMonoid, IntNatPlus())
+IntMonoid = migrate_theory(NatPlusMonoid, IntNatPlus())
 
 # Interpret `id` as reflexivity and `compose` as transitivity.
-IntPreorderCat = migrate(PreorderCat, IntPreorder())
+IntPreorderCat = migrate_theory(PreorderCat, IntPreorder())
 
 end # module
