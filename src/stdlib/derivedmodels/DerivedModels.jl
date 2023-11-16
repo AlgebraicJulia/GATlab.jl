90aaf930baf97e1d6bd392cdbc536e511052c190
Kevin Arlin <arlinkevind@gmail.com>
2023-11-16 10:41:44 -0800

    Fix merge error

--- src/stdlib/derivedmodels/DerivedModels.jl
+++ src/stdlib/derivedmodels/DerivedModels.jl
@@ -6,12 +6,12 @@ using ...StdTheoryMaps
 using ...StdModels
 
 # Given a model of a category C, we can derive a model of Cᵒᵖ.
-@migrate_model OpFinSetC = OpCat(FinSetC)
+OpFinSetC = migrate_model(OpCat, FinSetC())
 
 # Interpret `e` as `0` and `⋅` as `+`.
-@migrate_model IntMonoid = NatPlusMonoid(IntNatPlus)
+IntMonoid = migrate_model(NatPlusMonoid, IntNatPlus())
 
 # Interpret `id` as reflexivity and `compose` as transitivity.
-@migrate_model IntPreorderCat = PreorderCat(IntPreorder)
+IntPreorderCat = migrate_model(PreorderCat, IntPreorder())
 
 end # module
