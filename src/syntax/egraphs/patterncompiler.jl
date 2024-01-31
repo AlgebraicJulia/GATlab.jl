b1452991d968b6d41e34caece8c50a73ab286e03
Kris Brown <kris@topos.institute>
2024-01-30 16:36:42 -0800

    postmortem docstrings

--- src/syntax/egraphs/patterncompiler.jl
+++ src/syntax/egraphs/patterncompiler.jl
@@ -11,26 +11,37 @@ A Pattern is an AlgTerm `trm` in a context `ctx`.
 
 Patterns are compiled into programs that search for a morphism `f` from the
 context into the e-graph such that `f(trm)` also exists in the e-graph.
+
+TODO: enforce all terms in the context are used
 """
 struct Pattern
   ctx::GATContext
   trm::AlgTerm
 end
 
-struct Compiler
-  v2r::Dict{Ident, Reg}
+"""
+
+- `v2r`: a map of variables to registers
+- `free_vars`: indexed according to pattern vector of eterms, gives free variables
+- `subtree_size`: indexed according to pattern vector of eterms, gives size of term
+- `todo_nodes`: ???
+- `instructions`: instructions which have been emitted so far
+- `next_reg`: next free register
+"""
+struct CompileState
+  v2r::Dict{Ident,Reg}
   free_vars::Vector{Set{Ident}}
   subtree_size::Vector{Int}
-  todo_nodes::Dict{Tuple{Ident, Reg}, ETerm}
+  todo_nodes::Dict{Tuple{Ident,Reg},ETerm}
   instructions::Vector{Instruction}
   next_reg::Reg
   theory::GAT
-  function Compiler(theory::GAT)
+  function CompileState(theory::GAT)
     new(
-      Dict{Ident, Reg}(),
+      Dict{Ident,Reg}(),
       Set{Ident}[],
       Int[],
-      Dict{Tuple{Int, Reg}, ETerm}(),
+      Dict{Tuple{Int,Reg},ETerm}(),
       Instruction[],
       Reg(1),
       theory
@@ -38,20 +49,30 @@ struct Compiler
   end
 end
 
+"""
+Takes a term, produces a vector of E-terms where the last e-term corresponds 
+to the overall term. EIDs refer to indices within the vector. Returns the EID
+of the overall term.
 
+This is basically just a mini-egraph.
+"""
 function trm_to_vec!(trm::AlgTerm, vec::Vector{ETerm})
   ids = Vector{EId}(trm_to_vec!.(trm.args, Ref(vec)))
   push!(vec, ETerm(trm.head, ids))
   length(vec)
 end
 
+"""Inverse to trm_to_vec"""
 function vec_to_trm(vec::Vector{ETerm}, id::EId)
   etrm = vec[id]
-  args = Vector{AlgTerm}(vec_to_term(Ref(vec), etrm.args))
+  args = Vector{AlgTerm}(vec_to_term.(Ref(vec), etrm.args))
   AlgTerm(etrm.head, args)
 end
 
-function load_pattern!(c::Compiler, patvec::Vector{ETerm})
+"""
+Computes the set of free variables for each term, and the size of the subtree for each term.
+"""
+function load_pattern!(c::CompileState, patvec::Vector{ETerm})
   n = length(patvec)
 
   for etrm in patvec
@@ -72,7 +93,7 @@ function load_pattern!(c::Compiler, patvec::Vector{ETerm})
   end
 end
 
-function add_todo!(c::Compiler, patvec::Vector{ETerm}, id::EId, reg::Reg)
+function add_todo!(c::CompileState, patvec::Vector{ETerm}, id::EId, reg::Reg)
   etrm = patvec[id]
   hd = etrm.head
   if is_context(hd)
@@ -118,8 +139,8 @@ end
 # in c.v2r
 #
 # Why? Idk, this is how it works in egg
-function next!(c::Compiler)
-  function key(idreg::Tuple{EId, Reg})
+function next!(c::CompileState)
+  function key(idreg::Tuple{EId,Reg})
     id = idreg[1]
     n_bound = length(filter(v -> v in keys(c.v2r), c.free_vars[id]))
     n_free = length(c.free_vars[id]) - n_bound
@@ -132,10 +153,10 @@ function next!(c::Compiler)
   end
   v = c.todo_nodes[k]
   delete!(c.todo_nodes, k)
-  (k,v)
+  (k, v)
 end
 
-is_ground_now(c::Compiler, id::EId) = all(v ∈ keys(c.v2r) for v in c.free_vars[id])
+is_ground_now(c::CompileState, id::EId) = all(v ∈ keys(c.v2r) for v in c.free_vars[id])
 
 function extract(patvec::Vector{ETerm}, i::EId)
   trm = vec_to_trm(patvec, i)
@@ -149,7 +170,7 @@ function compile(mod::Module, pat::Pattern)
   patvec = ETerm[]
   trm_to_vec!(pat.trm, patvec)
 
-  c = Compiler(mod.Meta.theory)
+  c = CompileState(mod.Meta.theory)
 
   load_pattern!(c, patvec)
 
@@ -160,19 +181,25 @@ function compile(mod::Module, pat::Pattern)
   while true
     @match next!(c) begin
       nothing => break
+      # I think etrm = patvec[id]?
       ((id, reg), etrm) => begin
+        # If we've already bound all of the free variables of patvec[id]
+        # then we can just lookup what patvec[id] should be
         if is_ground_now(c, id) && (length(etrm.args) != 0)
           extracted = extract(patvec, id)
           push!(
             c.instructions,
             Lookup(
-              Union{ETerm, Reg}[is_context(t.head) ? c.v2r[t.head] : t for t in extracted],
+              Union{ETerm,Reg}[is_context(t.head) ? c.v2r[t.head] : t for t in extracted],
               reg
             )
           )
         else
           out = next_out
           next_out += length(etrm.args)
+          # Loop through all e-terms with head etrm.head
+          # put their id in reg,
+          # and their arguments in `out+1:out+length(etrm.args)`
           push!(
             c.instructions,
             Bind(
@@ -182,6 +209,8 @@ function compile(mod::Module, pat::Pattern)
             )
           )
           for (i, child) in enumerate(etrm.args)
+            # Note that this only actually adds the todo if the argument is a
+            # composite term not if the argument is an identifier
             add_todo!(c, patvec, child, out + i)
           end
         end
@@ -191,6 +220,6 @@ function compile(mod::Module, pat::Pattern)
 
   Program(
     c.instructions,
-    Reg[r for (v,r) in sort([c.v2r...]; by=vr -> index(vr[1]))]
+    Reg[r for (v, r) in sort([c.v2r...]; by=vr -> index(vr[1]))]
   )
 end
