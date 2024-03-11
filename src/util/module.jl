4f55d30487e6feeecf8ecaecc2d8582ac0a3f47b
Matt <cuffaro.matt@gmail.com>
2024-03-11 15:22:14 -0400

    @gatdoc macro factored out
    We removed @gatdoc in favor for using an overloaded Base.repr during
    theory creation. We also append the theory definition to the theory's
    docstring.

--- src/util/module.jl
+++ src/util/module.jl
@@ -5,11 +5,9 @@ using Reexport
 include("MetaUtils.jl")
 include("HashColor.jl")
 include("Eithers.jl")
-include("Gatdoc.jl")
 
 @reexport using .MetaUtils
 @reexport using .HashColor
 @reexport using .Eithers
-@reexport using .Gatdoc
 
 end
