b525d52b2db0a6b374de301a7d2578d6449cc87e
Kris Brown <kris@topos.institute>
2025-02-06 19:33:31 -0800

    Remove Model, change migration
    typecon too
    
    allow subtyping in WithModel
    
    Change how import works.
    
    remove redundant Using Gatlab
    
    .
    
    .
    
    fix docstrings
    
    .
    Migrate models with theorymap modules, not theorymaps
    Explicit dispatch
    
    working struct Wrapper
    
    remove Wrapper abstract type
    
    As a macro
    
    Allow docstrings on wrapper struct declarations
    
    Typed wrapper structs
    
    Optional typed wrapper constructor where type parameters are provided
    
    Weaken

--- src/nonstdlib/module.jl
+++ src/nonstdlib/module.jl
@@ -4,12 +4,8 @@ using Reexport
 
 include("theories/module.jl")
 include("models/module.jl")
-# include("theorymaps/module.jl")
-# include("derivedmodels/module.jl")
 
 @reexport using .NonStdTheories
 @reexport using .NonStdModels
-# @reexport using .StdTheoryMaps
-# @reexport using .StdDerivedModels
 
 end
